8f83cc99d68c3bbdf89b1b3c921cc81d180fa1b7
Faiz Mokhtar <mfmokhtar@gmail.com>
2020-12-27 10:58:33 +0800

    Initial Commit

---
+++ ToyCustomPopup/ToyCustomPopupApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToyCustomPopupApp.swift
+//  ToyCustomPopup
+//
+//  Created by Faiz Mokhtar AD0502 on 27/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct ToyCustomPopupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
